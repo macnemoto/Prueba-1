f179b80234135c2165e393ef85167fbd2670203c
macnemoto <marcosj465@gmail.com>
2024-03-24 14:35:32 -0400

    improvement in responsive design

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react'
 import './App.css'
+import { useEffect, useState } from 'react'
 import { type PokemonCard } from './types/ApiResponse'
 import cardPokemon from './data/cardPokemon.json'
 import Statistics from './components/statistics'
@@ -8,7 +8,7 @@ function App () {
   const [card, setCard] = useState<PokemonCard | null>(null)
 
   useEffect(() => {
-    fetch('https://api.pokemontcg.io/v2/cards/base6-18', {
+    fetch('https://api.pokemontcg.io/v2/cards/dp3-77', {
       headers: {
         'X-Api-Key': import.meta.env.VITE_API_KEY
       }
@@ -23,10 +23,9 @@ function App () {
   }, [])
 
   return (
-    <main className=' '>
-      <div className=' flex flex-col justify-center items-center px-4'>
+    <main className='flex flex-col lg:flex-row justify-center items-center lg:items-start'>
         <img className='w-64 my-3 rounded-lg' src={card?.data.images.large} alt="Pokémon Bulbasaur Card" />
-        <div className='flex flex-col items-center justify-between w-full font-semibold sm:w-1/2 md:w-1/2 lg:w-2/5 xl:w-1/3 2xl:w-1/4'>
+      <section className=' flex flex-col items-center justify-between w-full font-semibold md:w-[560px] px-4 lg:mt-3'>
           <p className='bg-slate-600 w-full rounded-t-lg p-4 text-white font-semibold text-2xl tracking-wide'>{card?.data.name}</p>
           <div className='flex w-full'>
             <div className='flex w-3/5 pb-2'>
@@ -40,22 +39,19 @@ function App () {
                 )}
               </div>
             </div>
-            <div className='flex flex-row justify-center items-center w-2/5 pb-3 bg-gray-500 rounded-bl-lg'>
+            <div className='flex flex-row justify-center items-center w-2/5 pb-3 bg-gray-500 rounded-bl-[9px]'>
               <p className='uppercase bg-gray-500 pr-2 '>ps <span className='text-2xl'> {card?.data.hp} </span></p>
               {card?.data.types.map((item, index) =>
-                <img className='h-6 w-6' key={index} src={`/img/${item}.webp`} alt="" />
+                <img className='h-6 w-6' key={index} src={`/img/${item}.webp`} alt={item} />
               )}
             </div>
           </div>
-        </div>
-      </div>
-      <div className='flex flex-col justify-center items-center'>
+          <div className=' flex flex-col my-3 w-full '>
         {card?.data.attacks.map((item, index) =>
           <Statistics key={index} name={item.name} power={item.damage} cost={item.cost} text={item.text} />
         )}
       </div>
-      <section className='flex justify-center items-center  w-full'>
-        <div className='flex flex-col items-center justify-between w-full md:w-1/2 lg:w-2/5 xl:w-1/3 2xl:w-1/4 px-4 md:px-0 '>
+      <div className='w-full mb-4'>
           <div className='text-center grid grid-cols-3 gap-4 bg-gray-300 w-full text-sm text-gray-800 rounded-t-lg px-4 pt-2'>
             <div className=''>
               <p>Debilidad</p>
@@ -77,7 +73,6 @@ function App () {
                       <span className='text-2xl text-black'>{item.value}</span>
                     </div>
                   ))
-
                   : <p></p>}
               </div>
             </div>
@@ -97,10 +92,8 @@ function App () {
             <a className='font-semibold text-blue-700' href="https://pokemon.fandom.com/es/wiki/Bulbasaur_(TCG)">Carta de promoción</a>
             <p className='font-medium' >SWSH231 Promo</p>
           </div>
-        </div>
-      </section>
-      <div>
       </div>
+        </section>
     </main>
   )
 }
--- src/components/statistics.tsx
+++ src/components/statistics.tsx
@@ -7,8 +7,8 @@ export interface PokemonStatistics {
 
 function Statistics ({ name, power, cost, text }: PokemonStatistics) {
   return (
-    <section className='flex flex-col justify-center items-center w-full px-4 my-5 md:w-1/2 lg:w-2/5 xl:w-1/3 2xl:w-1/4 text-white'>
-      <div className='flex items-center justify-between w-full px-4 mb-4'>
+    <section className='flex flex-col justify-center items-center w-full text-white my-4'>
+      <div className='flex items-center justify-between w-full px-4 mb-1'>
         <div className='flex flex-row w-4/5 '>
           {cost?.map((item, index) =>
             <img key={index} className='w-6 h-6 mt-1 mx-1' src={`/img/${item}.webp`} alt={item} />
