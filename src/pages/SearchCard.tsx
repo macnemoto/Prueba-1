205aeca47e73a02b03e5096661038688099a19ab
macnemoto <marcosj465@gmail.com>
2024-06-02 01:12:48 -0400

    new features, add 404 page and page when no letters are found

--- src/pages/SearchCard.tsx
+++ src/pages/SearchCard.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 import { type Key, useEffect, useState } from 'react'
-import { useParams, Link, useNavigate } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import { type Datum } from './types/SearchCard'
 import { ScrollClear } from '../helper/ScrollClear'
+import NotFound from './NotFound'
 
 function SearchCard () {
   ScrollClear()
   const { name } = useParams()
   const [pokeCards, setPokeCards] = useState([])
   const [loader, setLoader] = useState(true)
-  const navigate = useNavigate()
 
   useEffect(() => {
     const fechData = async () => {
@@ -29,11 +29,6 @@ function SearchCard () {
         setPokeCards(data)
         console.log(data.length)
 
-        if (data.length === 0) {
-          navigate('/Prueba-1/card/no-found')
-          return
-        }
-
         if (name != null) {
           localStorage.setItem(name, JSON.stringify(data))
         }
@@ -50,33 +45,35 @@ function SearchCard () {
       ? <div className='flex justify-center items-center min-h-[600px]'>
         <div className='pokemon'></div>
       </div>
-      : <><div className='flex flex-col lg:flex-row lg:items-center mb-10 px-5 mt-5 gap-2  dark:text-white'>
-        <label htmlFor="">View as</label>
-        <select className='w-28 h-10 pl-2 pr-2 border border-zinc-950 rounded-md dark:text-white dark:bg-[#0D1117]'>
-          <option value="Images">Images</option>
-          <option value="List">List</option>
+      : pokeCards.length === 0
+        ? <NotFound title={"Hmmmm...we couldn't find any cards matching your search criteria!"} img={'/Prueba-1/noFound/nofound.png'}/>
+        : <><div className='flex flex-col lg:flex-row lg:items-center mb-10 px-5 mt-5 gap-2  dark:text-white'>
+      <label htmlFor="">View as</label>
+      <select className='w-28 h-10 pl-2 pr-2 border border-zinc-950 rounded-md dark:text-white dark:bg-[#0D1117]'>
+        <option value="Images">Images</option>
+        <option value="List">List</option>
+      </select>
+      <label htmlFor="">Sorted by</label>
+      <div className='flex h-10'>
+        <select className='w-32 pl-2 rounded-l-md border border-zinc-950 dark:text-white dark:bg-[#0D1117] '>
+          <option value="Name">Name</option>
+          <option value="released">Set/Number</option>
+          <option value="rarity">Rarity</option>
         </select>
-        <label htmlFor="">Sorted by</label>
-        <div className='flex h-10'>
-          <select className='w-32 pl-2 rounded-l-md border border-zinc-950 dark:text-white dark:bg-[#0D1117] '>
-            <option value="Name">Name</option>
-            <option value="released">Set/Number</option>
-            <option value="rarity">Rarity</option>
-          </select>
-          <select className='w-20 pl-2 rounded-r-md border border-zinc-950  dark:text-white dark:bg-[#0D1117] '>
-            <option value="Asc">Asc</option>
-            <option value="Desc">Desc</option>
-          </select>
-        </div>
+        <select className='w-20 pl-2 rounded-r-md border border-zinc-950  dark:text-white dark:bg-[#0D1117] '>
+          <option value="Asc">Asc</option>
+          <option value="Desc">Desc</option>
+        </select>
       </div>
-      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:lg:grid-cols-5 gap-5 xl:gap-10  mb-20 justify-items-center'>
-      {
-      pokeCards?.map((item: Datum, index: Key | null | undefined) => (
-  <Link key={index} to={`/Prueba-1/card/${item.id}`}>
-    <img key={index} width={245} height={342} src={item.images.small} alt={item.name} />
-  </Link>
-      ))}
-        </div></>}
+    </div>
+    <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:lg:grid-cols-5 gap-5 xl:gap-10  mb-20 justify-items-center'>
+    {
+    pokeCards?.map((item: Datum, index: Key | null | undefined) => (
+<Link key={index} to={`/Prueba-1/card/${item.id}`}>
+  <img key={index} width={245} height={342} src={item.images.small} alt={item.name} />
+</Link>
+    ))}
+      </div></> }
   </div>)
 }
 
